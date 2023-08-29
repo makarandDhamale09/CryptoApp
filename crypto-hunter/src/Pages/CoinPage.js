963948949ef5a9bac6bfbcbfc21925028c137523
Makarand Dhamale <makarand.dhamale09@gmail.com>
2023-08-29 21:59:55 +0530

    CoinPage side bar completed

--- crypto-hunter/src/Pages/CoinPage.js
+++ crypto-hunter/src/Pages/CoinPage.js
@@ -1,7 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { CryptoState } from "./../CryptoContext";
+import axios from "axios";
+import { SingleCoin } from "../config/api";
+import { Typography, makeStyles } from "@material-ui/core";
+import CoinInfo from "../components/CoinInfo";
+import HTMLReactParser from "html-react-parser";
 
 const CoinPage = () => {
-  return <div>CoinPage</div>;
+  const { id } = useParams();
+  const [coin, setCoin] = useState();
+
+  const { currency, symbol } = CryptoState();
+
+  const fetchCoin = async () => {
+    const { data } = await axios.get(SingleCoin(id));
+    setCoin(data);
+  };
+
+  console.log(coin);
+
+  useEffect(() => {
+    fetchCoin();
+  }, []);
+
+  const useStyles = makeStyles((theme) => ({
+    container: {
+      display: "Flex",
+      [theme.breakpoints.down("md")]: {
+        flexDirection: "column",
+        alignItems: "center",
+      },
+    },
+    sidebar: {
+      width: "30%",
+      [theme.breakpoints.down("md")]: {
+        width: "100%",
+      },
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      marginTop: 25,
+      borderRight: "2px solid grey",
+    },
+    heading: {
+      fontWeight: "bold",
+      marginBottom: 20,
+      fontFamily: "Montserrat",
+    },
+  }));
+
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.sidebar}>
+        <img
+          src={coin?.image.large}
+          alt={coin?.name}
+          height="200"
+          style={{ marginBottom: 20 }}
+        />
+        <Typography variant="h3" className={classes.heading}>
+          {coin?.name}
+        </Typography>
+        <Typography variant="subtitle1" className={classes.description}>
+          {coin?.description.en.split(". ")[0]}
+        </Typography>
+      </div>
+
+      {/*chart*/}
+      <CoinInfo coin={coin}></CoinInfo>
+    </div>
+  );
 };
 
 export default CoinPage;
